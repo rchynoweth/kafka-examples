c5d1aca02b49f9d072006853ca22eab7afca64c7
rchynoweth <ryan.chynoweth@databricks.com>
2022-08-22 12:35:33 -0700

    slight changes to configs

--- kafka-connect-msk/create-snowflake-sink.sh
+++ kafka-connect-msk/create-snowflake-sink.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 curl -i -X PUT -H  "Content-Type:application/json" \
-    http://kafka-connect:8083/connectors/MY_FAVORITE_CELEBRITIES_SINK_SNOWFLAKE/config \
+    http://kafka-connect:8083/connectors/ryan_chynoweth_kafka_test/config \
     -d '{
         "connector.class":"com.snowflake.kafka.connector.SnowflakeSinkConnector",
         "tasks.max":1,
-        "topics":"twitter.my.favorite.celebrities.src",
-        "snowflake.url.name":"${file:/opt/confluent/secrets/connect-secrets.properties:SNOWFLAKE_HOST}",
-        "snowflake.user.name":"${file:/opt/confluent/secrets/connect-secrets.properties:SNOWFLAKE_USER}",
-        "snowflake.user.role":"SYSADMIN",
-        "snowflake.private.key":"${file:/opt/confluent/secrets/connect-secrets.properties:SNOWFLAKE_PRIVATE_KEY}",
-        "snowflake.database.name":"DEMO_DB",
-        "snowflake.schema.name":"PUBLIC",
+        "topics":"ryan_chynoweth_kafka_test",
+        "snowflake.url.name":"${file:connect-secrets.properties:SNOWFLAKE_HOST}",
+        "snowflake.user.name":"${file:connect-secrets.properties:SNOWFLAKE_USER}",
+        "snowflake.user.role":"ACCOUNTADMIN",
+        "snowflake.private.key":"${file:connect-secrets.properties:SNOWFLAKE_PRIVATE_KEY}",
+        "snowflake.database.name":"DEMO",
+        "snowflake.schema.name":"RAC_SCHEMA",
         "key.converter":"org.apache.kafka.connect.storage.StringConverter",
         "value.converter.schemas.enable": "false",
         "value.converter":"org.apache.kafka.connect.json.JsonConverter"
